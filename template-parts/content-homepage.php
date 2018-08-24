02fe19a4567780ee56d3824cdd723b832f34b1c0
davemoz <davejmoz@gmail.com>
2018-08-24 16:53:32 -0400

    More changes

--- template-parts/content-homepage.php
+++ template-parts/content-homepage.php
@@ -1,32 +1,29 @@
 <?php
+
 /**
- * The template used for displaying page content in page.php
+ * The template used for displaying page content in page-home.php
  *
  * @package davejmoz
  */
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="background-image: url( <?php the_post_thumbnail_url('original'); ?> ); background-size: cover; background-position: center center;">
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<?php echo project_categories_terms_links(); ?>
-	</header><!-- .entry-header -->
+<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="background-image: url( <?php the_post_thumbnail_url('original'); ?> ); background-size: cover; background-position: center center; background-repeat: no-repeat;">
+		<header class="entry-header">
+			<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+			<?php
+		$categories_list = get_the_category_list(__(', ', 'davejmoz'));
+		if ('' != $categories_list) {
+			$categories_text = 'Posted in: %1$s';
+		} else {
 
-	<?php /*
-	<div class="entry-content">
-		<?php the_excerpt(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'davejmoz' ),
-				'after'  => '</div>',
-			) );
+		}
+		printf($categories_text, $categories_list);
 		?>
-	</div><!-- .entry-content -->
-	*/ ?>
-
-	<footer class="entry-footer">
-		<?php edit_post_link( esc_html__( 'Edit', 'davejmoz' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
-
+		</header><!-- .entry-header -->
+		<footer class="entry-footer">
+			<?php // edit_post_link( esc_html__( 'Edit', 'davejmoz' ), '<span class="edit-link">', '</span>' ); ?>
+		</footer><!-- .entry-footer -->
+	</article><!-- #post-## -->
+</a>
